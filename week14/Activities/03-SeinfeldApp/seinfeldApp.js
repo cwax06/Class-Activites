875346a301bd2839e404ed77b9b43e5882f23c71
Andrew Jones <andrew@andrews-mbp-2.uconnect.utah.edu>
2017-12-18 18:36:52 -0700

    Monday

--- week14/Activities/03-SeinfeldApp/seinfeldApp.js
+++ week14/Activities/03-SeinfeldApp/seinfeldApp.js
@@ -1,1 +1,27 @@
 var express = require("express");
+var mysql = require("mysql");
+
+var app = express();
+
+var port = 3000;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "seinfeld_db"
+});
+
+app.get("/cast", function(req, res) {
+    connection.query("SELECT *FROM actors order by id", function (err, result) {
+        var html = "<h1>Actors Ordered BY ID<h1>";
+
+        html += "<ul>";
+
+        for (var i=0; i < result.length; i++) {
+            html += "<li><p> ID: " + result[i].id + "</p>";
+            html += "<li><p> Name: " + result[i].name + "</p>";
+            html += "<li><p> ID: " + result[i]. + "</p>";
+        }
+    })
+})
