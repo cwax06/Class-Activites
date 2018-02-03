8a1677bc7fae51d633a957377bd3dd55d6700d71
Andrew Jones <andrew@andrews-mbp-2.uconnect.utah.edu>
2018-02-03 09:05:18 -0700

    another day

--- week18/Activities/15-User-Schema/Unsolved/userModel.js
+++ week18/Activities/15-User-Schema/Unsolved/userModel.js
@@ -8,16 +8,51 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema({
   /* TODO:
    * Add four entries into our schema. These should be:
-   *
+   */
+  string: {
+    type: String,
+    trim: true,
+    required: "String is Required"
+  },
 
-   * 1: username: A string that will be be required, and also trimmed.
-   * 2: password: A string that will be required, trimmed, and at least 6 characters.
-   * 3: email: A string that must be a valid email address and unique in our collection.
-   * 4: userCreated: A date that will default to the current date.
-   *
-   * TIP: The regex for checking if a string is an email is: /.+\@.+\..+/
-   * Use that with the model attribute that checks for a valid match.
-   * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
+  number: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+
+  email: {
+    type: String,
+    match: [/.+\@.+\..+/, "Please enter a valid e-mail address"]
+  },
+
+  boolean: Boolean,
+
+  array: Array,
+
+  date: {
+    type: Date,
+    default: Date.now
+  },
+
+  longstring: {
+    type: String,
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      // Error Message
+      "Longstring should be longer."
+    ]
+  }
+  //  * 1: username: A string that will be be required, and also trimmed.
+  //  * 2: password: A string that will be required, trimmed, and at least 6 characters.
+  //  * 3: email: A string that must be a valid email address and unique in our collection.
+  //  * 4: userCreated: A date that will default to the current date.
+  //  *
+  //  * TIP: The regex for checking if a string is an email is: /.+\@.+\..+/
+  //  * Use that with the model attribute that checks for a valid match.
+  //  * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
 });
 
 // This creates our model from the above schema, using mongoose's model method
